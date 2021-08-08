df5c27f626b0fbb1573c2023598e4c6d8112d15d
Aubrey <aubreypacker5@gmail.com>
2021-08-08 12:13:26 -0700

    testing broken app changes updates

--- broken-app/app.js
+++ broken-app/app.js
@@ -1,18 +1,51 @@
 const express = require('express');
+const ExpressError = require("./expressError");
 let axios = require('axios');
 var app = express();
 
+app.use(express.json());
+
+function getUsers(userIn) {
+  let users = userIn.map(async userData => {
+    res = await axios.get(`https://api.github.com/users/${userData}`);
+    paredUser = { bio: res.data.bio, name: res.data.name}
+    console.log("paredUser", paredUser)
+    return paredUser
+  })
+  users.then(function(response) {console.log(response)})
+}
+
 app.post('/', function(req, res, next) {
   try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+    //  req.body.developers // ['joelburton', 'elie']
+    userIn = req.body.developers
+    getUsers(userIn)
+    // users // currently a promise 
+    // return users.send(JSON.stringify(output));
 
-    return res.send(JSON.stringify(out));
-  } catch {
+  } catch(err) {
     next(err);
   }
 });
 
+
+/** 404 handler */
+
+app.use(function (req, res, next) {
+  return new ExpressError("Not Found", 404);
+});
+
+/** general error handler */
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err.message,
+  });
+});
+
+module.exports = app;
+
 app.listen(3000);
+console.log("listening on 3000")
